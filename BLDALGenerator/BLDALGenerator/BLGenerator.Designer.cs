a39b781ffbaefc4f31bab1c11140d2878377f0ee
dsuraj <deshpande.suraj@gmail.com>
2013-08-10 13:09:38 +0530

    Initial commit

---
+++ BLDALGenerator/BLDALGenerator/BLGenerator.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace BLDALGenerator
+{
+    partial class BLGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BLGenerator));
+            this.btnGenerateBL = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblDatabase = new System.Windows.Forms.Label();
+            this.ddlDatabase = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ddlTable = new System.Windows.Forms.ComboBox();
+            this.lblTable = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.btnSaveto = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerateBL
+            // 
+            this.btnGenerateBL.Location = new System.Drawing.Point(105, 314);
+            this.btnGenerateBL.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerateBL.Name = "btnGenerateBL";
+            this.btnGenerateBL.Size = new System.Drawing.Size(269, 42);
+            this.btnGenerateBL.TabIndex = 8;
+            this.btnGenerateBL.Text = "Generate BL Class";
+            this.toolTip1.SetToolTip(this.btnGenerateBL, "Generate business logic class");
+            this.btnGenerateBL.UseVisualStyleBackColor = true;
+            this.btnGenerateBL.Click += new System.EventHandler(this.btnGenerateBL_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.lblServer);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.lblPassword);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.lblID);
+            this.groupBox1.Location = new System.Drawing.Point(14, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(494, 131);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MS SQL Database Authentication";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(249, 66);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(179, 26);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.toolTip1.SetToolTip(this.btnConnect, "Connect to SQL Server Database");
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(312, 29);
+            this.txtServer.Margin = new System.Windows.Forms.Padding(4);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(116, 21);
+            this.txtServer.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txtServer, "SQL Server instance name");
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(246, 32);
+            this.lblServer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(45, 15);
+            this.lblServer.TabIndex = 4;
+            this.lblServer.Text = "Server:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(91, 66);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(116, 21);
+            this.txtPassword.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txtPassword, "Password");
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(10, 66);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 15);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(91, 32);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(116, 21);
+            this.txtID.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txtID, "User ID");
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(7, 32);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(51, 15);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "User ID:";
+            // 
+            // lblDatabase
+            // 
+            this.lblDatabase.AutoSize = true;
+            this.lblDatabase.Location = new System.Drawing.Point(7, 30);
+            this.lblDatabase.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDatabase.Name = "lblDatabase";
+            this.lblDatabase.Size = new System.Drawing.Size(63, 15);
+            this.lblDatabase.TabIndex = 6;
+            this.lblDatabase.Text = "Database:";
+            // 
+            // ddlDatabase
+            // 
+            this.ddlDatabase.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlDatabase.FormattingEnabled = true;
+            this.ddlDatabase.Location = new System.Drawing.Point(78, 25);
+            this.ddlDatabase.Margin = new System.Windows.Forms.Padding(4);
+            this.ddlDatabase.Name = "ddlDatabase";
+            this.ddlDatabase.Size = new System.Drawing.Size(166, 23);
+            this.ddlDatabase.TabIndex = 5;
+            this.ddlDatabase.SelectedIndexChanged += new System.EventHandler(this.ddlDatabase_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ddlTable);
+            this.groupBox2.Controls.Add(this.lblTable);
+            this.groupBox2.Controls.Add(this.lblDatabase);
+            this.groupBox2.Controls.Add(this.ddlDatabase);
+            this.groupBox2.Location = new System.Drawing.Point(14, 169);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(494, 77);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // ddlTable
+            // 
+            this.ddlTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlTable.FormattingEnabled = true;
+            this.ddlTable.Location = new System.Drawing.Point(312, 25);
+            this.ddlTable.Margin = new System.Windows.Forms.Padding(4);
+            this.ddlTable.Name = "ddlTable";
+            this.ddlTable.Size = new System.Drawing.Size(166, 23);
+            this.ddlTable.TabIndex = 6;
+            this.ddlTable.SelectedIndexChanged += new System.EventHandler(this.ddlTable_SelectedIndexChanged);
+            // 
+            // lblTable
+            // 
+            this.lblTable.AutoSize = true;
+            this.lblTable.Location = new System.Drawing.Point(262, 25);
+            this.lblTable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(41, 15);
+            this.lblTable.TabIndex = 8;
+            this.lblTable.Text = "Table:";
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(105, 268);
+            this.txtFolderPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(269, 21);
+            this.txtFolderPath.TabIndex = 10;
+            // 
+            // btnSaveto
+            // 
+            this.btnSaveto.Location = new System.Drawing.Point(382, 263);
+            this.btnSaveto.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSaveto.Name = "btnSaveto";
+            this.btnSaveto.Size = new System.Drawing.Size(88, 26);
+            this.btnSaveto.TabIndex = 7;
+            this.btnSaveto.Text = "Save to";
+            this.toolTip1.SetToolTip(this.btnSaveto, "Select a folder where generated BL and DAL classes are stored.");
+            this.btnSaveto.UseVisualStyleBackColor = true;
+            this.btnSaveto.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 372);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(528, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.IsLink = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(68, 17);
+            this.toolStripStatusLabel1.Text = "I tweet here";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.IsLink = true;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(414, 17);
+            this.toolStripStatusLabel3.Spring = true;
+            this.toolStripStatusLabel3.Text = "Please send any feedbacks, suggestions or bugs here.";
+            this.toolStripStatusLabel3.Click += new System.EventHandler(this.toolStripStatusLabel3_Click);
+            // 
+            // BLGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 394);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSaveto);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGenerateBL);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "BLGenerator";
+            this.Text = "Business Layor Class Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateBL;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ComboBox ddlDatabase;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox ddlTable;
+        private System.Windows.Forms.Label lblTable;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Button btnSaveto;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ BLDALGenerator/BLDALGenerator/BLGenerator.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BLDALGenerator
+{
+    public partial class BLGenerator : Form
+    {
+        #region "Properties"
+
+        private string ProperCaseFileName
+        {
+            get
+            {
+                if (ddlTable.SelectedItem != null)
+                {
+                    return (char.ToUpper(Convert.ToString(ddlTable.SelectedItem)[0]) + Convert.ToString(ddlTable.SelectedItem).Substring(1)).Replace(" ", string.Empty);
+                }
+                return string.Empty;
+            }
+        }
+
+        #endregion "Properties"
+
+        public BLGenerator()
+        {
+            InitializeComponent();
+
+            ddlDatabase.Enabled = false;
+            ddlTable.Enabled = false;
+            txtID.Focus();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtID.Text.Trim()) && !string.IsNullOrEmpty(txtPassword.Text.Trim()) && !string.IsNullOrEmpty(txtServer.Text.Trim()))
+            {
+                FillDatabase();
+            }
+            else
+            {
+                MessageBox.Show("ID/Password/Server Name can't be blank.");
+            }
+        }
+
+        private void FillDatabase()
+        {
+            //Get DB List
+
+            var connectionString = string.Format("Data Source={0};User ID={1};Password={2};", txtServer.Text.Trim(), txtID.Text.Trim(), txtPassword.Text.Trim());
+
+            DataTable databases = null;
+            ddlDatabase.Items.Clear();
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                try
+                {
+                    databases = sqlConnection.GetSchema("Databases");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                sqlConnection.Close();
+            }
+
+            if (databases != null && databases.Rows.Count > 0)
+            {
+                foreach (DataRow database in databases.Rows)
+                {
+                    String databaseName = database.Field<String>("database_name");
+                    ddlDatabase.Items.Add(databaseName);
+                }
+
+                ddlDatabase.Enabled = true;
+                ddlDatabase.SelectedIndex = 0;
+                FillTables();
+            }
+        }
+
+        private void FillTables()
+        {
+            //Get Table list
+
+            var connectionString = string.Format("Data Source={0};User ID={1};Password={2};Initial Catalog={3};", txtServer.Text.Trim(), txtID.Text.Trim(), txtPassword.Text.Trim(), ddlDatabase.SelectedItem);
+            ddlTable.Items.Clear();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                try
+                {
+                    DataTable table = connection.GetSchema("Tables");
+                    if (table != null && table.Rows.Count > 0)
+                    {
+                        foreach (DataRow row in table.Rows)
+                        {
+                            if (row[2] != null && Convert.ToString(row["Table_Type"]).ToLower() == "base table")
+                            {
+                                string tableName = row[2].ToString();
+                                ddlTable.Items.Add(tableName);
+                                ddlTable.Enabled = true;
+                                ddlTable.SelectedIndex = 0;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private string GetColumns()
+        {
+            //Get column list
+
+            var connectionString = string.Format("Data Source={0};User ID={1};Password={2};Initial Catalog={3};", txtServer.Text.Trim(), txtID.Text.Trim(), txtPassword.Text.Trim(), ddlDatabase.SelectedItem);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                try
+                {
+                    DataTable table = connection.GetSchema("Columns", new[] { Convert.ToString(ddlDatabase.SelectedItem), null, Convert.ToString(ddlTable.SelectedItem) });
+                    if (table != null && table.Rows.Count > 0)
+                    {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.AppendFormat("public class {0}BL{1}{2}{3}", ProperCaseFileName, Environment.NewLine, "{", Environment.NewLine);
+
+                        stringBuilder.AppendFormat("{0}#region \"Properties\"{1}{2}", Environment.NewLine, Environment.NewLine, Environment.NewLine);
+                        foreach (DataRow row in table.Rows)
+                        {
+                            if (row["Column_Name"] != null && !string.IsNullOrEmpty(Convert.ToString(row["Column_Name"])) && row["Is_Nullable"] != null && !string.IsNullOrEmpty(Convert.ToString(row["Is_Nullable"])) && row["Data_Type"] != null && !string.IsNullOrEmpty(Convert.ToString(row["Data_Type"])))
+                            {
+                                string columnName = Convert.ToString(row["Column_Name"]);
+                                string nullableDataType = MapNullableDataType(Convert.ToString(row["Data_Type"]), Convert.ToString(row["Is_Nullable"]));
+
+                                stringBuilder.AppendFormat("public {0} {1}{2}{3}{4}get;{5}set;{6}", nullableDataType, columnName, Environment.NewLine, "{", Environment.NewLine, Environment.NewLine, Environment.NewLine);
+                                stringBuilder.Append("}");
+                                stringBuilder.Append(Environment.NewLine);
+                            }
+                        }
+                        stringBuilder.AppendFormat("{0}#endregion \"Properties\"{1}", Environment.NewLine, Environment.NewLine);
+                        stringBuilder.AppendFormat("{0}#region \"Methods\"{1}{2}#endregion \"Methods\"{3}", Environment.NewLine, Environment.NewLine, Environment.NewLine, Environment.NewLine);
+                        stringBuilder.Append("}");
+                        return stringBuilder.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            return string.Empty;
+        }
+
+        private string MapNullableDataType(string dataType, string isNullable)
+        {
+            isNullable = string.Compare(isNullable, "yes", true) == 0 ? "?" : string.Empty;
+
+            if (string.Compare(dataType, "varchar", true) == 0 || string.Compare(dataType, "nvarchar", true) == 0 || string.Compare(dataType, "char", true) == 0 || string.Compare(dataType, "text", true) == 0 || string.Compare(dataType, "nchar", true) == 0 || string.Compare(dataType, "ntext", true) == 0)
+            {
+                return "string";
+            }
+            else if (string.Compare(dataType, "bit", true) == 0)
+            {
+                return string.Format("bool{0}", isNullable);
+            }
+            else if (string.Compare(dataType, "bigint", true) == 0 || string.Compare(dataType, "tinyint", true) == 0 || string.Compare(dataType, "int", true) == 0 || string.Compare(dataType, "smallint", true) == 0)
+            {
+                return string.Format("int{0}", isNullable);
+            }
+            else if (string.Compare(dataType, "decimal", true) == 0 || string.Compare(dataType, "money", true) == 0 || string.Compare(dataType, "numeric", true) == 0 || string.Compare(dataType, "float", true) == 0)
+            {
+                return string.Format("decimal{0}", isNullable);
+            }
+            else if (string.Compare(dataType, "date", true) == 0 || string.Compare(dataType, "datetime", true) == 0 || string.Compare(dataType, "datetime2", true) == 0 || string.Compare(dataType, "smalldatetime", true) == 0 || string.Compare(dataType, "time", true) == 0)
+            {
+                return string.Format("DateTime{0}", isNullable);
+            }
+            else if (string.Compare(dataType, "uniqueidentifier", true) == 0)
+            {
+                return string.Format("Guid{0}", isNullable);
+            }
+
+            return dataType;
+        }
+
+        private void ddlDatabase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillTables();
+        }
+
+        private void btnGenerateBL_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFolderPath.Text.Trim()))
+            {
+                using (StreamWriter writer = new StreamWriter(string.Format("{1}\\{0}BL.cs", ProperCaseFileName, txtFolderPath.Text), false))
+                {
+                    writer.WriteLine(GetColumns());
+                }
+
+                if (MessageBox.Show(string.Format("{0}BL class generated successfully. Do you want to open the file?", ProperCaseFileName), "Success", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.OK)
+                {
+                    Process.Start(string.Format("{1}\\{0}BL.cs", ProperCaseFileName, txtFolderPath.Text));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a folder to save the file.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK || folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.Yes)
+            {
+                txtFolderPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void ddlTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnGenerateBL.Text = "Generate BL Class";
+            if (ddlTable.SelectedItem != null)
+            {
+                btnGenerateBL.Text = string.Format("Generate {0}BL Class", ProperCaseFileName);
+            }
+        }
+
+        private void toolStripStatusLabel3_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://surajdeshpande.wordpress.com/2013/08/06/business-layer-class-generator/");
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://twitter.com/deshpandesuraj");
+        }
+    }
+}
