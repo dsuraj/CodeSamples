6ad38267ae7006435333a677beabdecdb5f443a4
dsuraj <deshpande.suraj@gmail.com>
2013-08-11 21:08:07 +0530

    CryptSharp code added

---
+++ CryptSharp/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CryptSharp;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnEncryptPassword_Click(object sender, EventArgs e)
+    {
+        string cryptedPassword = Crypter.Blowfish.Crypt(txtPassword.Text.Trim());
+
+        Response.Write(string.Format("Password encrypted successfully. Encrypted password is: {0}", cryptedPassword));
+    }
+}
